324526d881c669a58d7fd6358f029a18f7598981
Oscar Ostlund <oscar.ostlund@gmail.com>
2019-04-21 18:31:28 -0400

    Update Cmake

--- src/I2CIO.cpp
+++ src/I2CIO.cpp
@@ -25,29 +25,27 @@
 // @author F. Malpartida - fmalpartida@gmail.com
 // ---------------------------------------------------------------------------
 
-#include <inttypes.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
+#include "I2CIO.h"
+#include "smbus.h"
 #include <fcntl.h>
-#include <unistd.h>
+#include <inttypes.h>
 #include <linux/i2c-dev.h>
-#include "../include/I2CIO.h"
-#include "../include/smbus.h"
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // CLASS VARIABLES
 // ---------------------------------------------------------------------------
 
-
 // CONSTRUCTOR
 // ---------------------------------------------------------------------------
-I2CIO::I2CIO ( )
-{
-   _i2cAddr     = 0x0;
-   _dirMask     = 0xFF;    // mark all as INPUTs
-   _shadow      = 0x0;     // no values set
-   _initialised = false;
-   _fd = 0;
+I2CIO::I2CIO() {
+  _i2cAddr = 0x0;
+  _dirMask = 0xFF; // mark all as INPUTs
+  _shadow = 0x0;   // no values set
+  _initialised = false;
+  _fd = 0;
 }
 
 // PUBLIC METHODS
@@ -55,124 +53,103 @@ I2CIO::I2CIO ( )
 
 //
 // begin
-int I2CIO::begin ( const char *device, uint8_t i2cAddr )
-{
-   int r;
+int I2CIO::begin(const char *device, uint8_t i2cAddr) {
+  int r;
 
-   _i2cAddr = i2cAddr;
+  _i2cAddr = i2cAddr;
 
-   _fd = open (device, O_RDWR);
-   if (_fd<0)
-     return false;
+  _fd = open(device, O_RDWR);
+  if (_fd < 0)
+    return false;
 
-   r=ioctl (_fd, I2C_SLAVE, i2cAddr);
-   if (r<0) {
-     close(_fd);
-     return false;
-   }
+  r = ioctl(_fd, I2C_SLAVE, i2cAddr);
+  if (r < 0) {
+    close(_fd);
+    return false;
+  }
 
-   _initialised = true;
-   return true;
+  _initialised = true;
+  return true;
 }
 
 //
 // pinMode
-void I2CIO::pinMode ( uint8_t pin, uint8_t dir )
-{
-   if ( _initialised )
-   {
-      if ( OUTPUT == dir )
-      {
-         _dirMask &= ~( 1 << pin );
-      }
-      else
-      {
-         _dirMask |= ( 1 << pin );
-      }
-   }
+void I2CIO::pinMode(uint8_t pin, uint8_t dir) {
+  if (_initialised) {
+    if (OUTPUT == dir) {
+      _dirMask &= ~(1 << pin);
+    } else {
+      _dirMask |= (1 << pin);
+    }
+  }
 }
 
 //
 // portMode
-void I2CIO::portMode ( uint8_t dir )
-{
-   if ( _initialised )
-   {
-      if ( dir == INPUT )
-      {
-         _dirMask = 0xFF;
-      }
-      else
-      {
-         _dirMask = 0x00;
-      }
-   }
+void I2CIO::portMode(uint8_t dir) {
+  if (_initialised) {
+    if (dir == INPUT) {
+      _dirMask = 0xFF;
+    } else {
+      _dirMask = 0x00;
+    }
+  }
 }
 
 //
 // read
-uint8_t I2CIO::read ( void )
-{
-// XXX: Read disabled
- return 0;
+uint8_t I2CIO::read(void) {
+  // XXX: Read disabled
+  return 0;
 }
 
 //
 // write
-int I2CIO::write ( uint8_t value )
-{
-   int status = 0;
+int I2CIO::write(uint8_t value) {
+  int status = 0;
 
-   if ( _initialised )
-   {
-      // Only write HIGH the values of the ports that have been initialised as
-      // outputs updating the output shadow of the device
-	_shadow = ( value & ~(_dirMask) );
-	status=i2c_smbus_write_byte(_fd, _shadow);
-   }
-   return ( (status == 0) );
+  if (_initialised) {
+    // Only write HIGH the values of the ports that have been initialised as
+    // outputs updating the output shadow of the device
+    _shadow = (value & ~(_dirMask));
+    status = i2c_smbus_write_byte(_fd, _shadow);
+  }
+  return ((status == 0));
 }
 
 //
 // digitalRead
-uint8_t I2CIO::digitalRead ( uint8_t pin )
-{
-   uint8_t pinVal = 0;
+uint8_t I2CIO::digitalRead(uint8_t pin) {
+  uint8_t pinVal = 0;
 
-   // Check if initialised and that the pin is within range of the device
-   // -------------------------------------------------------------------
-   if ( ( _initialised ) && ( pin <= 7 ) )
-   {
-      // Remove the values which are not inputs and get the value of the pin
-      pinVal = this->read() & _dirMask;
-      pinVal = ( pinVal >> pin ) & 0x01; // Get the pin value
-   }
-   return (pinVal);
+  // Check if initialised and that the pin is within range of the device
+  // -------------------------------------------------------------------
+  if ((_initialised) && (pin <= 7)) {
+    // Remove the values which are not inputs and get the value of the pin
+    pinVal = this->read() & _dirMask;
+    pinVal = (pinVal >> pin) & 0x01; // Get the pin value
+  }
+  return (pinVal);
 }
 
 //
 // digitalWrite
-int I2CIO::digitalWrite ( uint8_t pin, uint8_t level )
-{
-   uint8_t writeVal;
-   int status = 0;
+int I2CIO::digitalWrite(uint8_t pin, uint8_t level) {
+  uint8_t writeVal;
+  int status = 0;
 
-   // Check if initialised and that the pin is within range of the device
-   // -------------------------------------------------------------------
-   if ( ( _initialised ) && ( pin <= 7 ) )
-   {
-      // Only write to HIGH the port if the port has been configured as
-      // an OUTPUT pin. Add the new state of the pin to the shadow
-      writeVal = ( 1 << pin ) & ~_dirMask;
-      if ( level == HIGH )
-      {
-         _shadow |= writeVal;
-      }
-      else
-      {
-         _shadow &= ~writeVal;
-      }
-      status = this->write ( _shadow );
-   }
-   return ( status );
+  // Check if initialised and that the pin is within range of the device
+  // -------------------------------------------------------------------
+  if ((_initialised) && (pin <= 7)) {
+    // Only write to HIGH the port if the port has been configured as
+    // an OUTPUT pin. Add the new state of the pin to the shadow
+    writeVal = (1 << pin) & ~_dirMask;
+    if (level == HIGH) {
+      _shadow |= writeVal;
+    } else {
+      _shadow &= ~writeVal;
+    }
+    status = this->write(_shadow);
+  }
+  return (status);
 }
--- src/LiquidCrystal.cpp
+++ src/LiquidCrystal.cpp
@@ -3,7 +3,7 @@
 // Copyright 2011 - Under creative commons license 3.0:
 //        Attribution-ShareAlike CC BY-SA
 //
-// This software is furnished "as is", without technical support, and with no 
+// This software is furnished "as is", without technical support, and with no
 // warranty, express or implied, as to its usefulness for any purpose.
 //
 // Thread Safe: No
@@ -12,10 +12,10 @@
 // @file LiquidCrystal.cpp
 // This file implements a basic liquid crystal library that comes as standard
 // in the Arduino SDK.
-// 
-// @brief 
+//
+// @brief
 // This is a basic implementation of the LiquidCrystal library of the
-// Arduino SDK. The original library has been reworked in such a way that 
+// Arduino SDK. The original library has been reworked in such a way that
 // this class implements the all methods to command an LCD based
 // on the Hitachi HD44780 and compatible chipsets using the parallel port of
 // the LCD (4 bit and 8 bit).
@@ -26,86 +26,77 @@
 //
 // @author F. Malpartida - fmalpartida@gmail.com
 // ---------------------------------------------------------------------------
+#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
-#include <inttypes.h>
 
-#include "../include/LiquidCrystal.h"
+#include "LiquidCrystal.h"
 
 // CONSTANT  definitions
 // ---------------------------------------------------------------------------
 #define LCD_NOBACKLIGHT 0xFF
 
 // LCD driver configuration (4bit or 8bit driver control)
-#define LCD_4BIT                1
-#define LCD_8BIT                0
+#define LCD_4BIT 1
+#define LCD_8BIT 0
 
 // STATIC helper functions
 // ---------------------------------------------------------------------------
 
-
 // CONSTRUCTORS
 // ---------------------------------------------------------------------------
 
-LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t enable,
-                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-                             uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
-{
-   init(LCD_8BIT, rs, 255, enable, d0, d1, d2, d3, d4, d5, d6, d7);
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t enable, uint8_t d0, uint8_t d1,
+                             uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5,
+                             uint8_t d6, uint8_t d7) {
+  init(LCD_8BIT, rs, 255, enable, d0, d1, d2, d3, d4, d5, d6, d7);
 }
 
-LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
-                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-                             uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
-{
-   init(LCD_8BIT, rs, rw, enable, d0, d1, d2, d3, d4, d5, d6, d7);
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable, uint8_t d0,
+                             uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4,
+                             uint8_t d5, uint8_t d6, uint8_t d7) {
+  init(LCD_8BIT, rs, rw, enable, d0, d1, d2, d3, d4, d5, d6, d7);
 }
 
-LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
-                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3)
-{
-   init(LCD_4BIT, rs, rw, enable, d0, d1, d2, d3, 0, 0, 0, 0);
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable, uint8_t d0,
+                             uint8_t d1, uint8_t d2, uint8_t d3) {
+  init(LCD_4BIT, rs, rw, enable, d0, d1, d2, d3, 0, 0, 0, 0);
 }
 
-LiquidCrystal::LiquidCrystal(uint8_t rs,  uint8_t enable,
-                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3)
-{
-   init(LCD_4BIT, rs, 255, enable, d0, d1, d2, d3, 0, 0, 0, 0);
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t enable, uint8_t d0, uint8_t d1,
+                             uint8_t d2, uint8_t d3) {
+  init(LCD_4BIT, rs, 255, enable, d0, d1, d2, d3, 0, 0, 0, 0);
 }
 
 // Contructors with backlight control
-LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t enable,
-                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-                             uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7,
-                             uint8_t backlightPin, t_backlighPol pol)
-{
-   init(LCD_8BIT, rs, 255, enable, d0, d1, d2, d3, d4, d5, d6, d7);
-   setBacklightPin ( backlightPin, pol );
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t enable, uint8_t d0, uint8_t d1,
+                             uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5,
+                             uint8_t d6, uint8_t d7, uint8_t backlightPin,
+                             t_backlighPol pol) {
+  init(LCD_8BIT, rs, 255, enable, d0, d1, d2, d3, d4, d5, d6, d7);
+  setBacklightPin(backlightPin, pol);
 }
 
-LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
-                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-                             uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7,
-                             uint8_t backlightPin, t_backlighPol pol)
-{
-   init(LCD_8BIT, rs, rw, enable, d0, d1, d2, d3, d4, d5, d6, d7);
-   setBacklightPin ( backlightPin, pol );
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable, uint8_t d0,
+                             uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4,
+                             uint8_t d5, uint8_t d6, uint8_t d7,
+                             uint8_t backlightPin, t_backlighPol pol) {
+  init(LCD_8BIT, rs, rw, enable, d0, d1, d2, d3, d4, d5, d6, d7);
+  setBacklightPin(backlightPin, pol);
 }
 
-LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
-                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-                             uint8_t backlightPin, t_backlighPol pol)
-{
-   init(LCD_4BIT, rs, rw, enable, d0, d1, d2, d3, 0, 0, 0, 0);
-   setBacklightPin ( backlightPin, pol );
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable, uint8_t d0,
+                             uint8_t d1, uint8_t d2, uint8_t d3,
+                             uint8_t backlightPin, t_backlighPol pol) {
+  init(LCD_4BIT, rs, rw, enable, d0, d1, d2, d3, 0, 0, 0, 0);
+  setBacklightPin(backlightPin, pol);
 }
 
-LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t enable,
-                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-                             uint8_t backlightPin, t_backlighPol pol)
-{
-   init(LCD_4BIT, rs, 255, enable, d0, d1, d2, d3, 0, 0, 0, 0);
-   setBacklightPin ( backlightPin, pol );
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t enable, uint8_t d0, uint8_t d1,
+                             uint8_t d2, uint8_t d3, uint8_t backlightPin,
+                             t_backlighPol pol) {
+  init(LCD_4BIT, rs, 255, enable, d0, d1, d2, d3, 0, 0, 0, 0);
+  setBacklightPin(backlightPin, pol);
 }
 
 // PUBLIC METHODS
@@ -114,43 +105,29 @@ LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t enable,
 /************ low level data pushing commands **********/
 //
 // send
-void LiquidCrystal::send(uint8_t value, uint8_t mode) 
-{
-}
+void LiquidCrystal::send(uint8_t value, uint8_t mode) {}
 
 //
 // setBacklightPin
-void LiquidCrystal::setBacklightPin ( uint8_t pin, t_backlighPol pol )
-{
-}
+void LiquidCrystal::setBacklightPin(uint8_t pin, t_backlighPol pol) {}
 
 //
 // setBackligh
-void LiquidCrystal::setBacklight ( uint8_t value )
-{
-}
+void LiquidCrystal::setBacklight(uint8_t value) {}
 
 // PRIVATE METHODS
 // ---------------------------------------------------------------------------
 
-
 // init
-void LiquidCrystal::init(uint8_t fourbitmode, uint8_t rs, uint8_t rw, uint8_t enable,
-                         uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-                         uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
-{
-}
+void LiquidCrystal::init(uint8_t fourbitmode, uint8_t rs, uint8_t rw,
+                         uint8_t enable, uint8_t d0, uint8_t d1, uint8_t d2,
+                         uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6,
+                         uint8_t d7) {}
 
 //
 // pulseEnable
-void LiquidCrystal::pulseEnable(void) 
-{
-}
+void LiquidCrystal::pulseEnable(void) {}
 
 //
 // write4bits
-void LiquidCrystal::writeNbits(uint8_t value, uint8_t numBits) 
-{
-}
-
-
+void LiquidCrystal::writeNbits(uint8_t value, uint8_t numBits) {}
